e3aa04e554cc49715d56315e10420b3987837b90
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-12-05 09:10:54 +1100

    Fix dependent option

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :comments, dependent: destroy
+  has_many :comments, dependent: :destroy
 
   validates :user, presence: true
-  
+
   validates :title, presence: true
   validates :link, presence: true
   validates :body, presence: true
