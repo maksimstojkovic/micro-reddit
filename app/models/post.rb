d1f1fd6c9a58f4278287b87f0033ee20c3fc1abe
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-12-05 09:06:07 +1100

    Add post model

---
+++ app/models/post.rb
@@ -0,0 +1,10 @@
+class Post < ApplicationRecord
+  belongs_to :user
+  has_many :comments, dependent: destroy
+
+  validates :user, presence: true
+  
+  validates :title, presence: true
+  validates :link, presence: true
+  validates :body, presence: true
+end
